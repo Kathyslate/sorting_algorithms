c269587a4e13633b517a0f369a58c5c16179e714
Kathyslate <mercyoyetunde98@gmail.com>
2024-02-19 05:43:18 -0800

    wert

--- sort.h
+++ sort.h
@@ -34,5 +34,6 @@ void change(int *array, int item1, int item2);
 void cocktail_sort_list(listint_t **list);
 void exchange(listint_t **head, listint_t *node1, listint_t *node2);
 void counting_sort(int *array, size_t size);
+void *my_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
