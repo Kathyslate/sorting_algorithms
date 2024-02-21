7c96771577f2dbb73e295ab9d77ad0363c90f279
Kathyslate <mercyoyetunde98@gmail.com>
2024-02-21 02:11:52 -0800

    jnjn

--- deck.h
+++ deck.h
@@ -1,19 +1,6 @@
 #ifndef DECK_H
 #define DECK_H
 
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
 
 /**
  * enum kind_e - card kind
@@ -43,6 +30,29 @@ typedef struct card_s
 	const kind_t kind;
 } card_t;
 
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+        const card_t *card;
+        struct deck_node_s *prev;
+        struct deck_node_s *next;
+} deck_node_t;
+
+
 void sort_deck(deck_node_t **deck);
+int _strcmp(const char *str1, char *str2);
+int get_card(deck_node_t *node);
+deck_node_t *swap_card(deck_node_t *card, deck_node_t **deck);
+void insertion_sort_deck(deck_node_t **deck);
+
+
+
 
 #endif
